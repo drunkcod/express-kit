37c78613ee3800d0adf3bac305a47ef448270a45
Tobbe Gyllebring <tobbe@hubert.ai>
2025-02-23 20:57:12 +0100

    Error handling examples added.

--- examples/starter/app.ts
+++ examples/starter/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerShutdown } from '@drunkcod/express-kit';
+import { asyncHandler, registerShutdown } from '@drunkcod/express-kit';
 
 const PORT = 8080;
 
@@ -10,8 +10,37 @@ const PORT = 8080;
 		res.send('hello world');
 	});
 
+	// ✅ 500 and error as "expected"
+	app.use('/error/plain', (req, res) => {
+		throw new Error('Plain error.');
+	});
+
+	// ❌ unobserved rejection will kill the process.
+	app.use('/error/async', async (req, res) => {
+		await Promise.reject(new Error('Async error.'));
+	});
+
+	// ✅ the express4 way to do it
+	app.use('/error/async-plain', async (req, res, next) => {
+		try {
+			await Promise.reject(new Error('Async error.'));
+		} catch (error) {
+			next(error);
+		}
+	});
+
+	// ✅ using express-kit
+	app.use(
+		'/error/async-kit',
+		asyncHandler(async (req: express.Request, res: express.Response) => {
+			await Promise.reject(new Error('Async error.'));
+		})
+	);
+
 	const server = app.listen(PORT, () => console.log("I'm listening..."));
+	// chandle ctrl+c SIGKILL/SIGABRT
 	registerShutdown(server, async () => {
+		//close and db connections or other event loop keep alives here.
 		console.log('bye...');
 	});
 })().catch(console.error);
--- examples/starter/docker-start.sh
+++ examples/starter/docker-start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -lt --rm sha256:1c4f916df977fd0905add8140e0913cc5f5c0d2a78718584dbb03c34969b4b13
+docker run -lt --rm -p 8080:8080 sha256:1c4f916df977fd0905add8140e0913cc5f5c0d2a78718584dbb03c34969b4b13
