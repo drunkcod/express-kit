70c0e9a2b6b765f414ffbde8d1e92444a2661ed5
Tobbe Gyllebring <tobbe@hubert.ai>
2025-02-24 09:56:15 +0100

    Simplified typing and make improve common case type inferrence.

--- express-async/asyncHandler.spec.ts
+++ express-async/asyncHandler.spec.ts
@@ -1,135 +1,140 @@
-import { describe, it, expect } from '@jest/globals';
-import { AsyncBinder, asyncHandler, boundAsyncHandler } from './index.js'
+import { describe, it, test, expect } from '@jest/globals';
+import { AsyncBinder, asyncHandler, boundAsyncHandler } from './index.js';
 import express from 'express';
 
 describe('asyncHandler', () => {
-    it('adds next arg for simple handler', () => {
-        const handler = async (req: express.Request, res: express.Response) => {};
-        const it = asyncHandler(handler);
-        expect(it.length).toEqual(3);
-    });
+	it('adds next arg for simple handler', () => {
+		const handler = async (req: express.Request, res: express.Response) => {};
+		const it = asyncHandler(handler);
+		expect(it.length).toEqual(3);
+	});
 
-    it('supports error handler', () => {
-        const handler = async (error: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {};
-        const it = asyncHandler(handler);
-        expect(it.length).toEqual(4);
-    });
+	it('supports error handler', () => {
+		const handler = async (error: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {};
+		const it = asyncHandler(handler);
+		expect(it.length).toEqual(4);
+	});
 
-    it('supports custom handler', async () => {
-        const handler = async (req: express.Request & { value: number }, res: express.Response) => req.value;
-        const it = asyncHandler(handler);
-        expect(it.length).toEqual(3);
-        const any = { value: 42 } as any;
-        expect(await it(any, any, any)).toEqual(any.value);
-    });
+	test('inferrence', () => {
+		const handler = asyncHandler(async (req, res) => {});
+		const error = asyncHandler(async (error: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {});
+	});
 
+	it('supports custom handler', async () => {
+		const handler = async (req: express.Request & { value: number }, res: express.Response) => req.value;
+		const it = asyncHandler(handler);
+		expect(it.length).toEqual(3);
+		const any = { value: 42 } as any;
+		expect(await it(any, any, any)).toEqual(any.value);
+	});
 
-    it('catches non async promise problems', async () => {
-        const any = {} as any;
-        const handler = (req: express.Request, res: express.Response): Promise<void> => {
-            throw new Error();
-        }
+	it('catches non async promise problems', async () => {
+		const any = {} as any;
+		const handler = (req: express.Request, res: express.Response): Promise<void> => {
+			throw new Error();
+		};
 
-        const it = asyncHandler(handler);
-        let caughtError = null;
-        await it(any, any, (error) => { caughtError = error;});
+		const it = asyncHandler(handler);
+		let caughtError = null;
+		await it(any, any, (error) => {
+			caughtError = error;
+		});
 
-        expect(caughtError).not.toBeNull();
-    })
-
+		expect(caughtError).not.toBeNull();
+	});
 });
 
 class MyController {
-    value: object | number | undefined;
+	value: object | number | undefined;
 
-    async getValue(req: express.Request, res: express.Response) {
-        return this.value;
-    }
+	async getValue(req: express.Request, res: express.Response) {
+		return this.value;
+	}
 
-    async getT(req: express.Request<{value: number}>, res: express.Response, next: express.NextFunction) {
-        return req.params.value;
-    }
+	async getT(req: express.Request<{ value: number }>, res: express.Response, next: express.NextFunction) {
+		return req.params.value;
+	}
 
-    async getCustom(req: express.Request & {value: number}, res: express.Response, next: express.NextFunction) {
-        return req.value;
-    }
+	async getCustom(req: express.Request & { value: number }, res: express.Response, next: express.NextFunction) {
+		return req.value;
+	}
 
-    async onError(err: Error, req: express.Request, res: express.Response, next: express.NextFunction) {
-        return err;
-    }
+	async onError(err: Error, req: express.Request, res: express.Response, next: express.NextFunction) {
+		return err;
+	}
 }
 
 describe('boundAsyncHandler', () => {
-    it('binds to instance', async () => {
-        const controller = new MyController();
-        controller.value = 42;
-        const it = boundAsyncHandler(controller, 'getValue');
-        const any = {} as any;
-        expect(it.length).toEqual(3);
-        expect(await it(any, any, any)).toEqual(controller.value);
-    });
+	it('binds to instance', async () => {
+		const controller = new MyController();
+		controller.value = 42;
+		const it = boundAsyncHandler(controller, 'getValue');
+		const any = {} as any;
+		expect(it.length).toEqual(3);
+		expect(await it(any, any, any)).toEqual(controller.value);
+	});
 
-    it('supports typed requests', async () => {
-        const controller = new MyController();
-        const it = boundAsyncHandler(controller, 'getT');
-        const any = { params: { value: 42 } } as any;
-        expect(it.length).toEqual(3);
-        expect(await it(any, any, any)).toEqual(any.params.value);
-    });
+	it('supports typed requests', async () => {
+		const controller = new MyController();
+		const it = boundAsyncHandler(controller, 'getT');
+		const any = { params: { value: 42 } } as any;
+		expect(it.length).toEqual(3);
+		expect(await it(any, any, any)).toEqual(any.params.value);
+	});
 
-    it('supports extended requests', async () => {
-        const controller = new MyController();
-        const it = boundAsyncHandler(controller, 'getCustom');
-        const any = { value: 42 } as any;
-        expect(it.length).toEqual(3);
-        expect(await it(any, any, any)).toEqual(any.value);
-    });
+	it('supports extended requests', async () => {
+		const controller = new MyController();
+		const it = boundAsyncHandler(controller, 'getCustom');
+		const any = { value: 42 } as any;
+		expect(it.length).toEqual(3);
+		expect(await it(any, any, any)).toEqual(any.value);
+	});
 
-    it('supports error handler', async () => {
-        const controller = new MyController();
-        const it = boundAsyncHandler(controller, 'onError');
-        const any = { } as any;
-        const error = new Error();
-        expect(it.length).toEqual(4);
-        expect(await it(error, any, any, any)).toEqual(error);
-    });
+	it('supports error handler', async () => {
+		const controller = new MyController();
+		const it = boundAsyncHandler(controller, 'onError');
+		const any = {} as any;
+		const error = new Error();
+		expect(it.length).toEqual(4);
+		expect(await it(error, any, any, any)).toEqual(error);
+	});
 
-    interface CustomRequest extends express.Request {
-        customValue: any;
-    }
+	interface CustomRequest extends express.Request {
+		customValue: any;
+	}
 
-    class CustomRequestController {
-        async getValue(req: CustomRequest, res: express.Response) {
-            return req.customValue;
-        }
-    }
+	class CustomRequestController {
+		async getValue(req: CustomRequest, res: express.Response) {
+			return req.customValue;
+		}
+	}
 
-    it('supports custom request types', async () => {
-        const controller = new CustomRequestController();
-        const it = boundAsyncHandler(controller, 'getValue');
-        const any = { customValue: 'custom-value' } as any;
-        expect(it.length).toEqual(3);
-        expect(await it(any, any, any)).toEqual(any.customValue);
-    });    
+	it('supports custom request types', async () => {
+		const controller = new CustomRequestController();
+		const it = boundAsyncHandler(controller, 'getValue');
+		const any = { customValue: 'custom-value' } as any;
+		expect(it.length).toEqual(3);
+		expect(await it(any, any, any)).toEqual(any.customValue);
+	});
 });
 
 describe('AsyncBinder', () => {
-    const controller = new MyController();
-    controller.value = 42;
-    const binder = new AsyncBinder(controller);
+	const controller = new MyController();
+	controller.value = 42;
+	const binder = new AsyncBinder(controller);
 
-    it('binds to instance', async () => {
-        const it = binder.bind('getValue');
-        const any = {} as any;
-        expect(it.length).toEqual(3);
-        expect(await it(any, any, any)).toEqual(controller.value);
-    });
+	it('binds to instance', async () => {
+		const it = binder.bind('getValue');
+		const any = {} as any;
+		expect(it.length).toEqual(3);
+		expect(await it(any, any, any)).toEqual(controller.value);
+	});
 
-    it('supports error handler', async () => {
-        const it = binder.bind('onError');
-        const any = { } as any;
-        const error = new Error();
-        expect(it.length).toEqual(4);
-        expect(await it(error, any, any, any)).toEqual(error);
-    });
+	it('supports error handler', async () => {
+		const it = binder.bind('onError');
+		const any = {} as any;
+		const error = new Error();
+		expect(it.length).toEqual(4);
+		expect(await it(error, any, any, any)).toEqual(error);
+	});
 });
--- express-async/index.ts
+++ express-async/index.ts
@@ -1,23 +1,19 @@
-import type express from 'express'
+import type express from 'express';
 
 type WithReturn<Type extends (...args: any) => any, R> = Type extends (...args: infer Args) => any ? (...args: Args) => R : never;
 type IsEmptyObject<T extends Record<PropertyKey, unknown>> = [keyof T] extends [never] ? true : false;
 
-type RequestHandler<T, Req extends express.Request<any> = express.Request<any>> = 
-    ((req: Req, res: express.Response<any>) => Promise<T>)
-    | ((req: Req, res: express.Response<any>, next: express.NextFunction) => Promise<T>)
+type RequestHandler<T, Req extends express.Request<any> = express.Request<any>> =
+	| ((req: Req, res: express.Response<any>) => Promise<T>)
+	| ((req: Req, res: express.Response<any>, next: express.NextFunction) => Promise<T>);
 
-type RequestParams = 
-      [express.Request<any>, express.Response] 
-    | [express.Request<any>, express.Response, express.NextFunction];
+type RequestParams = [express.Request<any>, express.Response] | [express.Request<any>, express.Response, express.NextFunction];
 
 type ErrorParams = [Error, express.Request<any>, express.Response, express.NextFunction];
 
-type IsRequestHandler<T, P extends keyof T> = 
-    T[P] extends (...args:any) => any ? Parameters<T[P]> extends RequestParams ? P : never : never;
+type IsRequestHandler<T, P extends keyof T> = T[P] extends (...args: any) => any ? (Parameters<T[P]> extends RequestParams ? P : never) : never;
 
-type IsErrorHandler<T, P extends keyof T> =
-    T[P] extends (...args:any) => any ? Parameters<T[P]> extends ErrorParams ? P : never : never
+type IsErrorHandler<T, P extends keyof T> = T[P] extends (...args: any) => any ? (Parameters<T[P]> extends ErrorParams ? P : never) : never;
 
 type ErrorHandler<T> = WithReturn<express.ErrorRequestHandler, Promise<T>>;
 
@@ -27,72 +23,70 @@ type HandlerErrorFns<C> = { [P in keyof C as IsErrorHandler<C, P>]: C[P] };
 type ControllerHandlerFns<C> = IsEmptyObject<HandlerFns<C>> extends true ? never : HandlerFns<C>;
 type ControllerErrorFns<C> = IsEmptyObject<HandlerErrorFns<C>> extends true ? never : HandlerErrorFns<C>;
 
-type ControllerFns<C> = ControllerHandlerFns<C> | ControllerErrorFns<C>
+type ControllerFns<C> = ControllerHandlerFns<C> | ControllerErrorFns<C>;
 
-type Unwrap<T> = T extends Promise<infer U> ? U : T; 
+type Unwrap<T> = T extends Promise<infer U> ? U : T;
 
 const safeResolve = <Fn extends (...args: any) => any>(fn: Fn, ...args: Parameters<Fn>) => {
-    type ReturnT = Unwrap<ReturnType<Fn>>
-    try {
-        return Promise.resolve<ReturnT>(fn(...args))
-    } catch(reason) {
-        return Promise.reject<ReturnT>(reason);
-    }
+	type ReturnT = Unwrap<ReturnType<Fn>>;
+	try {
+		return Promise.resolve<ReturnT>(fn(...args));
+	} catch (reason) {
+		return Promise.reject<ReturnT>(reason);
+	}
+};
+
+interface AsyncFn<P extends [...any], T> {
+	(...args: [...P]): Promise<T>;
 }
 
-type HandlerFn<Req, T>  = 
-    Req extends [never] ? never :
-    ((...args: [request: Req, response: express.Response]) => Promise<T>) 
-    | ((...args: [request: Req, response: express.Response, next: express.NextFunction]) => Promise<T>)
+type HandlerParameters<R extends express.Request> = [request: R, response: express.Response, next: express.NextFunction];
 
-type ErrorHandlerFn<Req, T>  = 
-    Req extends [never] ? never :
-    ((...args: [errpr: Error, request: Req, response: express.Response, next: express.NextFunction]) => Promise<T>) 
-
-type IsFn<T, P extends keyof T> = T[P] extends (...args:any) => any ? P : never;
+type IsFn<T, P extends keyof T> = T[P] extends (...args: any) => any ? P : never;
 type Fns<T> = { [P in keyof T as IsFn<T, P>]: T[P] };
 
 export const as = <T>(x: T) => x;
 export function bind<T extends Fns<T>, K extends keyof T>(target: T, fn: K): T[K] {
-    return as<Function>(target[fn]).bind(target);
+	return as<Function>(target[fn]).bind(target);
 }
 
-export function asyncHandler<T, Req extends express.Request = express.Request<any>>(fn: HandlerFn<Req, T>) :  RequestHandler<T> 
-export function asyncHandler<T, Req extends express.Request = express.Request<any>>(fn: ErrorHandlerFn<Req, T>) : ErrorHandler<T>
-export function asyncHandler<T>(fn: (...args:any) => Promise<T>): RequestHandler<any> | ErrorHandler<any> {
-    switch(fn.length) {
-        case 2:
-        case 3: return (req: express.Request, res: express.Response, next: express.NextFunction) => safeResolve(fn, req, res, next).catch(next);
+export function asyncHandler<T, Req extends express.Request>(fn: AsyncFn<HandlerParameters<Req>, T>): RequestHandler<T>;
+export function asyncHandler<T, Req extends express.Request>(fn: AsyncFn<[error: Error, ...HandlerParameters<Req>], T>): ErrorHandler<T>;
+export function asyncHandler<T>(fn: (...args: any) => Promise<T>): RequestHandler<any> | ErrorHandler<any> {
+	switch (fn.length) {
+		case 2:
+		case 3:
+			return (req: express.Request, res: express.Response, next: express.NextFunction) => safeResolve(fn, req, res, next).catch(next);
 
-        case 4: return (error: any, req: express.Request, res:express.Response, next: express.NextFunction) => 
-            safeResolve(fn, error, req, res, next).catch(next);
+		case 4:
+			return (error: any, req: express.Request, res: express.Response, next: express.NextFunction) => safeResolve(fn, error, req, res, next).catch(next);
 
-        default: return (...args:any[]) => {
-            const next = args.pop();
-            return safeResolve(fn, ...args, next).catch(next);
-        }
-    }
+		default:
+			return (...args: any[]) => {
+				const next = args.pop();
+				return safeResolve(fn, ...args, next).catch(next);
+			};
+	}
 }
 
 export function boundAsyncHandler<C extends ControllerHandlerFns<C>, T>(x: C, m: keyof ControllerHandlerFns<C>): RequestHandler<unknown>;
 export function boundAsyncHandler<C extends ControllerErrorFns<C>, T>(x: C, m: keyof ControllerErrorFns<C>): ErrorHandler<unknown>;
 export function boundAsyncHandler<C extends ControllerFns<C>>(x: C, m: keyof ControllerFns<C>): RequestHandler<unknown> | ErrorHandler<unknown> {
-    return asyncHandler(as<Function>(x[m]).bind(x));
+	return asyncHandler(as<Function>(x[m]).bind(x));
 }
 
 export class AsyncBinder<Controller extends ControllerFns<Controller>> {
+	static for<Controller extends ControllerFns<Controller>>(controller: Controller) {
+		const self = new AsyncBinder(controller);
+		return self.bind.bind(self);
+	}
 
-    static for<Controller extends ControllerFns<Controller>>(controller: Controller) {
-        const self = new AsyncBinder(controller);
-        return self.bind.bind(self);
-    }
+	constructor(private controller: Controller) {}
 
-    constructor(private controller: Controller) {}
-
-    bind(m: keyof ControllerHandlerFns<Controller>): RequestHandler<unknown>;
-    bind(m: keyof ControllerErrorFns<Controller>): ErrorHandler<unknown>;
-    bind(m: keyof ControllerFns<Controller>): RequestHandler<unknown> | ErrorHandler<unknown> {
-        const x = this.controller;
-        return asyncHandler(as<Function>(x[m]).bind(x));
-    }
+	bind(m: keyof ControllerHandlerFns<Controller>): RequestHandler<unknown>;
+	bind(m: keyof ControllerErrorFns<Controller>): ErrorHandler<unknown>;
+	bind(m: keyof ControllerFns<Controller>): RequestHandler<unknown> | ErrorHandler<unknown> {
+		const x = this.controller;
+		return asyncHandler(as<Function>(x[m]).bind(x));
+	}
 }
